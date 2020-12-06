728d147cc9327141e76b647653ac18bf8da2d3a7
Tomas Janousek <tomi@nomi.cz>
2020-12-06 22:02:32 +0000

    Record screen saver/locker status (X11)
    Tracking inactivity via idle time is unsuitable if one wants to account
    for activities such as watching movies or reading difficult texts.
    As media players usually keep the screen saver/locker from triggering,
    tracking inactivity that way might be more accurate, as long as the
    screen saver trigger is set to low enough duration or the user
    diligently locks their screen when walking away.
    
    Currently implemented for X11 only, and even that implementation
    supports only a few desktop environments.
    
    Relates: https://github.com/nomeata/arbtt/issues/39

--- src/DumpFormat.hs
+++ src/DumpFormat.hs
@@ -42,7 +42,8 @@ instance ToJSON (TimeLogEntry CaptureData) where
         "rate" .= tlRate,
         "inactive" .= cLastActivity tlData,
         "windows" .= map (\(a,t,p) -> object ["active" .= a, "title" .= t, "program" .= p]) (cWindows tlData),
-        "desktop" .= cDesktop tlData
+        "desktop" .= cDesktop tlData,
+        "screensaver" .= cScreenSaver tlData
         ]
 
 instance FromJSON (TimeLogEntry CaptureData) where
@@ -55,6 +56,7 @@ instance FromJSON (TimeLogEntry CaptureData) where
                 (,,) <$> v .: "active" <*> v .: "title" <*> v .: "program"
             ) . toList)
         cDesktop <- v .: "desktop"
+        cScreenSaver <- v .: "screensaver"
         let tlData = CaptureData {..}
         let entry = TimeLogEntry {..}
         pure entry
@@ -71,7 +73,7 @@ dumpActivity :: TimeLog (CaptureData, TimeZone, ActivityData) -> IO ()
 dumpActivity = mapM_ go
  where
     go tle = do
-        dumpHeader tz (tlTime tle) (cLastActivity cd)
+        dumpHeader tz (tlTime tle) (cLastActivity cd) (cScreenSaver cd)
         dumpDesktop (cDesktop cd)
         mapM_ dumpWindow (cWindows cd)
         dumpTags ad
@@ -82,11 +84,12 @@ dumpTags :: ActivityData -> IO ()
 dumpTags = mapM_ go
   where go act = printf "    %s\n" (show act)
 
-dumpHeader :: TimeZone -> UTCTime -> Integer -> IO ()
-dumpHeader tz time lastActivity = do
-    printf "%s (%dms inactive):\n"
+dumpHeader :: TimeZone -> UTCTime -> Integer -> Bool -> IO ()
+dumpHeader tz time lastActivity screenSaver = do
+    printf "%s (%dms inactive%s):\n"
         (formatTime defaultTimeLocale "%F %X" (utcToLocalTime tz time))
         lastActivity
+        (if screenSaver then ", screen saver/locker active" else [])
 
 dumpWindow :: (Bool, Text, Text) -> IO ()
 dumpWindow (active, title, program) = do
@@ -102,7 +105,7 @@ dumpDesktop d
 
 dumpSample :: TimeZone -> TimeLogEntry CaptureData -> IO ()
 dumpSample tz tle = do
-    dumpHeader tz (tlTime tle) (cLastActivity (tlData tle))
+    dumpHeader tz (tlTime tle) (cLastActivity (tlData tle)) (cScreenSaver (tlData tle))
     dumpDesktop (cDesktop (tlData tle))
     mapM_ dumpWindow (cWindows (tlData tle))
 
